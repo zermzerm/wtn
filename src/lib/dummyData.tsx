3faaef8d9649a2cafd8ee27807118e4e6965fbb2
zermzerm <zerm0907@gmail.com>
2025-06-25 16:51:13 +0900

    메인 페이지 리뷰순 인기순 정렬 구현

--- src/lib/dummyData.tsx
+++ src/lib/dummyData.tsx
@@ -4,3 +4,142 @@ export const swiperDummy = [
   { id: 3, title: "3번이미지", img: "/images/swiperImage.jpeg" },
   { id: 4, title: "4번이미지", img: "/images/swiperImage.jpeg" },
 ];
+
+export const totalTop = [
+  {
+    id: 1,
+    title: "전지적 독자 시점",
+    genre: "판타지",
+    author: "UMI",
+    star: 4.5,
+    review: 5432,
+    img: "/images/전지적독자시점.PNG",
+  },
+  {
+    id: 2,
+    title: "마음의 소리",
+    genre: "개그",
+    author: "조석",
+    star: 4.4,
+    review: 4321,
+    img: "/images/마음의 소리.PNG",
+  },
+  {
+    id: 3,
+    title: "재벌집 막내아들",
+    genre: "드라마",
+    author: "JP",
+    star: 4.3,
+    review: 3215,
+    img: "/images/재벌집막내.PNG",
+  },
+  {
+    id: 4,
+    title: "우렉 마지노",
+    genre: "판타지",
+    author: "SIU",
+    star: 4.2,
+    review: 2154,
+    img: "/images/우렉마지노.PNG",
+  },
+  {
+    id: 5,
+    title: "호랑이형님",
+    genre: "판타지",
+    author: "이상규",
+    star: 4.1,
+    review: 1543,
+    img: "/images/호랑이형님.PNG",
+  },
+];
+
+export const reviewTop = [
+  {
+    id: 1,
+    title: "호랑이형님",
+    genre: "판타지",
+    author: "이상규",
+    star: 4.1,
+    review: 1543,
+    img: "/images/호랑이형님.PNG",
+  },
+  {
+    id: 2,
+    title: "우렉 마지노",
+    genre: "판타지",
+    author: "SIU",
+    star: 4.2,
+    review: 2154,
+    img: "/images/우렉마지노.PNG",
+  },
+  {
+    id: 3,
+    title: "재벌집 막내아들",
+    genre: "드라마",
+    author: "JP",
+    star: 4.3,
+    review: 3215,
+    img: "/images/재벌집막내.PNG",
+  },
+  {
+    id: 4,
+    title: "마음의 소리",
+    genre: "개그",
+    author: "조석",
+    star: 4.4,
+    review: 4321,
+    img: "/images/마음의 소리.PNG",
+  },
+  {
+    id: 5,
+    title: "전지적 독자 시점",
+    genre: "판타지",
+    author: "UMI",
+    star: 4.5,
+    review: 5432,
+    img: "/images/전지적독자시점.PNG",
+  },
+];
+
+export const recentReview = [
+  {
+    id: 1,
+    name: "장난꾸러기",
+    image: "/svg/profile.png",
+    star: 5,
+    content:
+      "두번 정주행한 작품. 정신 나간 주인공의 정신 나간 여정과 주변에 있는 정신 나간 놈들이 어우러져 정신 나간 작품이 탄생했다. 글이 참 맛깔난다.",
+    title: "호랑이형님",
+    author: "이상규",
+  },
+  {
+    id: 2,
+    name: "장난꾸러기",
+    image: "/svg/profile.png",
+    star: 5,
+    content:
+      "두번 정주행한 작품. 정신 나간 주인공의 정신 나간 여정과 주변에 있는 정신 나간 놈들이 어우러져 정신 나간 작품이 탄생했다. 글이 참 맛깔난다.",
+    title: "호랑이형님",
+    author: "이상규",
+  },
+  {
+    id: 3,
+    name: "장난꾸러기",
+    image: "/svg/profile.png",
+    star: 5,
+    content:
+      "두번 정주행한 작품. 정신 나간 주인공의 정신 나간 여정과 주변에 있는 정신 나간 놈들이 어우러져 정신 나간 작품이 탄생했다. 글이 참 맛깔난다.",
+    title: "호랑이형님",
+    author: "이상규",
+  },
+  {
+    id: 4,
+    name: "장난꾸러기",
+    image: "/svg/profile.png",
+    star: 5,
+    content:
+      "두번 정주행한 작품. 정신 나간 주인공의 정신 나간 여정과 주변에 있는 정신 나간 놈들이 어우러져 정신 나간 작품이 탄생했다. 글이 참 맛깔난다.",
+    title: "호랑이형님",
+    author: "이상규",
+  },
+];
