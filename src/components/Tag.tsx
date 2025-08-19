abe5e4631f0ea2f0e7522ee36a870d17b8717b3e
zermzerm <zerm0907@gmail.com>
2025-08-19 12:43:50 +0900

    밑줄 애니메이션

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -24,8 +24,29 @@ const Section = styled.section`
 const Div = styled.div`
   color: #484848;
   font-size: 14px;
+  position: relative;
+  display: inline-block;
+  text-decoration: none;
+  padding-bottom: 10px;
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: 0; /* 글자 아래쪽에 위치 */
+    width: 100%;
+    height: 3px; /* 밑줄 두께 */
+    background: #23d2e2;
+    transform: translateY(8px) scaleY(0); /* 처음엔 보이지 않음 (세로축 0) */
+    transform-origin: bottom; /* 밑에서 시작 */
+    transition: transform 0.4s ease;
+  }
   &:hover {
     font-size: 15.5px;
     font-weight: 500;
+  }
+
+  &:hover::after {
+    transform: translateY(0) scaleY(1); /* 밑줄만 hover 시 채워짐 */
   }
 `;
